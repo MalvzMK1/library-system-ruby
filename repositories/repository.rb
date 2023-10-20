82ce95678fc260f991582c489256f987fc9aca3f
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-10-20 12:05:10 -0300

    Defining default repositories functions

--- repositories/repository.rb
+++ repositories/repository.rb
@@ -5,15 +5,34 @@ class Repository
     puts 'Repository initialized successefully!!!'
   end
 
-  def addNewBook(book)
-    @@books.push(book)
+  def find_book_index_by_id(book_id)
+    index = @@books.index { |book| book.get_infos[:id] == book_id }
+    return index
   end
 
-  def getAllBooks
+  def get_all_books
     return @@books
   end
 
-  def deleteBook(bookId)
-    
+  def get_book_by_id(book_id)
+    return @@books[find_book_index_by_id(book_id)]
+  end
+
+  def add_book(book)
+    @@books.push(book)
+    return @@books
+  end
+
+  def remove_book(book_id)
+    index = find_book_index_by_id(book_id)
+
+    if index > -1
+      @@books.delete_at(index)
+      return @@books
+    end
+  end
+
+  def update_book(book_id)
+    index = @@books.find_index(book)
   end
 end
