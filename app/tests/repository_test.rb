f507be40c23d334c0acd69133b689fb7c3401442
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-10-21 22:12:16 -0300

    Testing repositorie's get all books method

--- app/tests/repository_test.rb
+++ app/tests/repository_test.rb
@@ -50,17 +50,37 @@ class RepositoryTest < Minitest::Test
     refute_equal Book, @repository.get_book_by_id(book.get_infos[:id])
   end
 
-  # def test_get_book_by_id
-  #   book1 = Book.new("Book One", 200, 4.5, 1)
-  #   book2 = Book.new("Book Two", 250, 4.0, 2)
+  def test_get_book_by_id
+    book_one = Book.new('Book One', 200, 4.5, 1)
+    book_two = Book.new('Book Two', 250, 5, 2)
 
-  #   @repository.add_book(book1)
-  #   @repository.add_book(book2)
+    @repository.add_book(book_one)
+    @repository.add_book(book_two)
 
-  #   assert_equal book1, @repository.get_book_by_id(1)
-  #   assert_equal book2, @repository.get_book_by_id(2)
-  #   assert_equal nil, @repository.get_book_by_id(3)
-  # end
+    result = @repository.get_book_by_id(1)
+
+    assert_equal Book, result.class
+    assert_equal book_one.get_infos[:id], result.get_infos[:id]
+    assert_equal book_one, result
+  end
+
+  def test_get_all_books
+    book_one = Book.new('Book One', 200, 4.5, 1)
+    book_two = Book.new('Book Two', 250, 5, 2)
+    book_three = Book.new('Book Three', 400, 3, 3)
+
+    @repository.add_book(book_one)
+    @repository.add_book(book_two)
+    @repository.add_book(book_three)
+
+    result = @repository.get_all_books
+
+    assert_equal 3, result.length
+    for book in result
+      assert_equal Book, book.class
+    end
+  end
+
   describe 'My Class' do # Other way to do run tests
     it 'Does something' do 
       assert true
