f00033df18eea56d10245da20dab04457cd69790
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-10-22 00:00:21 -0300

    Entry point file to execute every test

---
+++ app/tests/consolidated_tests.rb
@@ -0,0 +1,13 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_group 'Tests', 'app/tests'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Repositories', 'app/repositories'
+  add_group 'Entry Point', 'app/main'
+end
+
+require_relative 'repository_test'
+require_relative 'book_test'
+require_relative 'controller_test'
