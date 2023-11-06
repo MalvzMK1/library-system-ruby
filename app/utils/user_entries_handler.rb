6bd251873de1c54fb1d1742fad6804912ce196c5
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-11-06 20:17:09 -0300

    user entries handler

---
+++ app/utils/user_entries_handler.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require_relative '../models/book'
+
+class UserEntriesHandler
+  def create_book_instance
+    puts "Insert book name: "
+    book_name = gets.to_s
+    puts "Insert book page amount: "
+    book_pages = gets.to_i
+    puts "Insert book's rating"
+    book_rating = gets
+
+    if book_rating.to_f == 0
+      book_rating = nil
+    else if book_rating.to_f < 0 || book_rating.to_f > 5
+           book_rating = nil
+         else
+           book_rating = book_rating.to_f
+         end
+    end
+
+    if book_pages == 0
+      throw Exception.new('Invalid pages amount')
+    end
+
+    Book.new book_name, book_pages, book_rating.nil? ? (0) : book_rating
+  end
+end
