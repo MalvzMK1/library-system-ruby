9559944fa9a57aa29db5d46b0e855f04f4c671d9
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-11-06 18:45:53 -0300

    Testing controller

--- app/tests/controller_test.rb
+++ app/tests/controller_test.rb
@@ -28,5 +28,25 @@ class ControllerTest < Minitest::Test
     book = LocalBook.new('Book One', 200)
 
     assert_equal LocalBook, book.class
+
+    @repository.add_book(book)
+
+    assert_equal book, @repository.get_book_by_id(1)
+  end
+
+  def test_get_all_books
+    book_one = LocalBook.new('Book One', 200)
+    book_two = LocalBook.new('Book Two', 200)
+    book_three = LocalBook.new('Book Three', 200)
+
+    @repository.add_book(book_one)
+    @repository.add_book(book_two)
+    @repository.add_book(book_three)
+
+    all_books = @repository.get_all_books
+
+    assert_equal 3, all_books.length
+    assert_equal book_one, @repository.get_book_by_id(1)
+    assert_equal book_two.get_infos, @repository.get_book_by_id(2).get_infos
   end
 end
